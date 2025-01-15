27d2393c833608675f841c6b514738284757de08
Simon Mika <simon@mika.se>
2025-01-15 16:39:39 +0100

    Migration working.

--- State/Client.ts
+++ State/Client.ts
@@ -9,7 +9,7 @@ export class Client {
 	async append(
 		data: storage.KeyValueStore.ListItem<any, any>[]
 	): Promise<storage.KeyValueStore.ListItem<any, any>[] | undefined> {
-		return undefined
+		return this.client.patch("", data)
 	}
 	async list(
 		options?: storage.KeyValueStore.ListOptions
